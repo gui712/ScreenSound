d69c68368362a8d616c0ac3c6a2be91a58a39716
Guilherme <guilhermecs_30@yahoo.com.br>
2024-01-25 14:48:52 -0300

    Criando menu

--- PrimeiroProjeto/PrimeiroProjeto/Program.cs
+++ PrimeiroProjeto/PrimeiroProjeto/Program.cs
@@ -19,11 +19,33 @@ void ExibirMensagem()
 
 void ExibirOpcoesDoMenu()
 {
-    Console.WriteLine("\nDigite 1 para registrar uma banda: ");
-    Console.WriteLine("Digite 2 para mostrar todas as bandas: ");
-    Console.WriteLine("Digite 3 Para Avaliar uma Banda: ");
-    Console.WriteLine("Digite 4 para exibir a nota média de uma banda: ");
-    Console.WriteLine("Digite 0 para sair :");
+    Console.WriteLine("\nDigite 1 para registrar uma banda ");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas ");
+    Console.WriteLine("Digite 3 Para Avaliar uma Banda ");
+    Console.WriteLine("Digite 4 para exibir a nota média de uma banda ");
+    Console.WriteLine("Digite 0 para sair");
+    
+    Console.Write("\nDigite uma opção: ");
+    string opcaoEscolhida = Console.ReadLine()!;
+    int opcaoEscolhidaNum = int.Parse(opcaoEscolhida);
+    
+    switch(opcaoEscolhidaNum)
+    {
+        case 1: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNum);
+            break;
+        case 2: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNum);
+            break;
+        case 3: Console.WriteLine("Você escolhue a opção " + opcaoEscolhidaNum);
+            break;
+        case 4:
+            Console.WriteLine("Você escolhue a opção " + opcaoEscolhidaNum);
+            break;
+        case 0: Console.WriteLine("Programado encerrado!!");
+            break;
+        default: Console.WriteLine("Opção inválida!!!");
+            break;
+
+    }
 }
 
 ExibirMensagem();
--- PrimeiroProjeto/PrimeiroProjeto/obj/Release/net6.0/PrimeiroProjeto.AssemblyInfo.cs
+++ PrimeiroProjeto/PrimeiroProjeto/obj/Release/net6.0/PrimeiroProjeto.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("PrimeiroProjeto")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3fbecc02da600795a30b87fbe3a9373aa8f6f075")]
 [assembly: System.Reflection.AssemblyProductAttribute("PrimeiroProjeto")]

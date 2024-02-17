85e59aae24b7bbadd8b55c7a7ad2751286ee8794
Guilherme <guilhermecs_30@yahoo.com.br>
2024-02-17 15:44:47 -0300

    Implementar Avaliacao

--- ScreenSound/Modelos/Banda.cs
+++ ScreenSound/Modelos/Banda.cs
@@ -1,9 +1,11 @@
-﻿namespace ScreenSoud.Modelos;
+﻿using ScreenSound.Modelos;
+
+namespace ScreenSoud.Modelos;
 
 internal class Banda
 {
     private List<Album> albuns = new List<Album>();
-    private List<int> notas = new List<int>();
+    private List<Avaliacao> notas = new List<Avaliacao>();
 
     public Banda(string nome)
     {
@@ -11,7 +13,19 @@ internal class Banda
     }
 
     public string Nome { get; }
-    public double Media => notas.Average();
+    public double Media
+    {
+        get
+        {
+            if(notas.Count == 0)
+            {
+                return 0;
+            } else
+            {
+                return notas.Average(a => a.Nota);
+            }
+        }
+    }
     public List<Album> Albuns => albuns;
 
     public void AdicionarAlbum(Album album) 
@@ -19,7 +33,7 @@ internal class Banda
         albuns.Add(album);
     }
 
-    public void AdicionarNota(int nota)
+    public void AdicionarNota(Avaliacao nota)
     {
         notas.Add(nota);
     }
